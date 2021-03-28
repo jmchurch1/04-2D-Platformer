b0c7662ae9ba31b3c8b9a8a71c3766722b15cd5b
jmchurch <jmchurch@iu.edu>
2021-03-28 19:08:01 -0400

    added a sick waterfall

--- Player/AnimatedSprite.gd
+++ Player/AnimatedSprite.gd
@@ -1,12 +1,11 @@
 extends AnimatedSprite
 
+onready var tween = get_node("Tween")
+var health = false
 
-#func _ready():
-#	connect("frame_changed", self, "_on_frame_changed")
 
-#func _on_frame_changed():
-#	match frame:
-#		1:
-#			$Footstep.play()
-
+func _process(_delta):
+	if Input.is_action_just_pressed("test"):
+		tween.interpolate_property(CanvasItem, "shader_param/progress", 0, 1, .5, Tween.TRANS_LINEAR, Tween.EASE_IN)
+		tween.start()
 
